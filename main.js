dc480bf8d97062ab33076fbfe255f357edc31dda
Douglas Badaró <douglas.badaro@outlook.com>
2022-11-06 08:28:00 -0300

    final commit

--- main.js
+++ main.js
@@ -21,20 +21,6 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) +
-  createCard(
-    "21/11",
-    "segunda",
-    createGame("england", "10:00", "iran") +
-      createGame("senegal", "13:00", "netherlands") +
-      createGame("unitedstates", "16:00", "wales")
-  ) +
-  createCard(
-    "22/11",
-    "terça",
-    createGame("argentina", "07:00", "saudiarabia") +
-      createGame("denmark", "10:00", "tunisia")
-  ) +
   createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
   createCard(
     "28/11",
